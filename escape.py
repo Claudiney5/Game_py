80a74505b3f77ef8bc7eee025d26f2449fb03ad9
Claudiney5 <claudiney.mail@gmail.com>
2020-06-14 12:33:40 -0300

    some errors

--- escape.py
+++ escape.py
@@ -12,7 +12,7 @@ class GameScreen():
         self.help_text = help_text
         self.required_item = required_item
 
-    def on_click(self, event, item_in_use):
+    def on_click(self, event, **item_in_use):
         if self.master.has_won:
         	return
 
@@ -55,6 +55,8 @@ class Game(tk.Tk):
         self.right_frame = tk.Frame(self, width=300, height=800)
         self.right_frame.pack_propagate(0)
 
+
+        # Variáveis de ajuda, informações e dica aos jogadores:
         self.help_var = tk.StringVar(self.right_frame)
         self.help_var.set("Tente clicar em algo.")
 
@@ -62,6 +64,20 @@ class Game(tk.Tk):
                                  padx=10, pady=20)
         self.help_box.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
+        self.help_history_var_1 = tk.StringVar(self.right_frame)
+        self.help_history_var_2 = tk.StringVar(self.right_frame)
+        self.help_history_var_3 = tk.StringVar(self.right_frame)
+
+        help_history_box_1 = tk.Label(self.right_frame, textvar=self.help_history_var_1, bg="black", fg="white", padx=10, pady=10)
+        help_history_box_2 = tk.Label(self.right_frame, textvar=self.help_history_var_2, bg="black", fg="white", padx=10, pady=10)
+        help_history_box_3 = tk.Label(self.right_frame, textvar=self.help_history_var_3, bg="black", fg="white", padx=10, pady=10)
+
+        help_history_box_1.pack(side=tk.TOP, fill=tk.X, padx=10)
+        help_history_box_2.pack(side=tk.TOP, fill=tk.X, padx=10)
+        help_history_box_3.pack(side=tk.TOP, fill=tk.X, padx=10)
+
+
+        # INVENTÁRIO:
         inventory_title = tk.Label(self.right_frame, text="Inventário: ", background="grey", foreground="white")
 
         inventory_space = tk.Frame(self.right_frame, background="lightgrey", width=300, height=320)
@@ -70,59 +86,142 @@ class Game(tk.Tk):
         inventory_space.pack(side=tk.BOTTOM)
         inventory_title.pack(side=tk.BOTTOM, fill=tk.X)
 
-        # colocando as imagens dentro de cada variável de inventário:
-        inventory_slot_1 = tk.Button(inventory_space, image=self.question_mark_image, width=50, height=50)
-        inventory_slot_2 = tk.Button(inventory_space, image=self.question_mark_image, width=50, height=50)
-        inventory_slot_3 = tk.Button(inventory_space, image=self.question_mark_image, width=50, height=50)
-         
-        # criando um espaço entre os botões de inventário
-        inventory_slot_1.pack(pady=(40, 20), padx=20)
-        inventory_slot_2.pack(pady=20, padx=20)
-        inventory_slot_3.pack(pady=(20, 0), padx=20)
+        inventory_row_1 = tk.Frame(inventory_space, pady=10)  
+        inventory_row_2 = tk.Frame(inventory_space, pady=10)
+        inventory_row_3 = tk.Frame(inventory_space, pady=10)
 
-        # inserindo na Lista de inventários os itens (variáveis de inventário):
+        # colocando as imagens dentro de cada variável de inventário:
+        inventory_slot_1 = tk.Button(inventory_row_1, 
+        	                         image=self.question_mark_image, 
+        	                         width=50, height=50,
+        	                         bg="lightgrey",
+        	                         command=lambda: self.use_item(0))
+        inventory_slot_2 = tk.Button(inventory_row_2, 
+        	                         image=self.question_mark_image, 
+        	                         width=50, height=50,
+        	                         bg="lightgrey",
+        	                         command=lambda: self.use_item(1))
+        inventory_slot_3 = tk.Button(inventory_row_3, 
+        	                         image=self.question_mark_image, 
+        	                         width=50, height=50,
+        	                         bg="lightgrey",
+        	                         command=lambda: self.use_item(2))
+
+        # inserindo na Lista de inventário os itens (variáveis de inventário):
+        item_name_1 = tk.StringVar(inventory_row_1)
+        item_name_2 = tk.StringVar(inventory_row_2)
+        item_name_3 = tk.StringVar(inventory_row_3)
+
+        self.item_label_vars = [item_name_1, item_name_2, item_name_3]
+
+        item_label_1 = tk.Label(inventory_row_1, textvar=item_name_1, anchor="w")
+        item_label_2 = tk.Label(inventory_row_2, textvar=item_name_2, anchor="w")
+        item_label_3 = tk.Label(inventory_row_3, textvar=item_name_3, anchor="w")
+
+        inventory_row_1.pack(fill=tk.X, expand=1)
+        inventory_row_2.pack(fill=tk.X, expand=1)
+        inventory_row_3.pack(fill=tk.X, expand=1)
+
+        # criando um espaço entre os botões de inventário e preenchendo o X
+        inventory_slot_1.pack(side=tk.LEFT, padx=(100, 20))
+        item_label_1.pack(side=tk.LEFT, fill=tk.X, expand=1)
+        inventory_slot_2.pack(side=tk.LEFT, padx=(100, 20))
+        item_label_2.pack(side=tk.LEFT, fill=tk.X, expand=1)
+        inventory_slot_3.pack(side=tk.LEFT, padx=(100, 20))
+        item_label_3.pack(side=tk.LEFT, fill=tk.X, expand=1)
+
         self.inventory_slots.append(inventory_slot_1)
         self.inventory_slots.append(inventory_slot_2)
         self.inventory_slots.append(inventory_slot_3)
-
 
         self.right_frame.pack(side=tk.RIGHT)
         self.screen.pack(side=tk.LEFT)
 
         # vinculando um método ao clicar na imagem principal:
-        self.screen.bind("<Button-1>", self.handle_click)
+        self.screen.bind("<Button-1>", self.handle_click, self.item_in_use)
 
-    def handle_click(self, event):
-        self.active_screen.on_click(event)
+
+    def handle_click(self, event, **item_in_use):
+        self.active_screen.on_click(event)      # @@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 
     # simplemente seta uma lista de objetos GameScreen como referência da claase Game para criar as telas do jogo:
     def set_game_screens(self, game_screens):
         self.game_screens = game_screens
 
+
     def display_screen(self, game_screen_number):
         self.active_screen = self.game_screens[game_screen_number]
         self.screen.delete("all")
         self.screen.create_image((250, 400), image=self.active_screen.image)
-        self.help_var.set(self.active_screen.help_text)
+        self.show_help_text(self.active_screen.help_text)
+
 
     def show_next_screen(self):
         self.current_screen_number += 1;
         if self.current_screen_number < len(self.game_screens):
             self.display_screen(self.current_screen_number)
+            self.clear_used_item()
         else:
             self.screen.delete("all")
             self.screen.configure(bg="black")
             self.screen.create_text((320, 300), text="Você venceu!!", font=self.sucess_font, fill="white")
+            self.has_won = True
+
+
+    def show_help_text(self, text):
+    	self.help_history_var_3.set(self.help_history_var_2.get())
+    	self.help_history_var_2.set(self.help_history_var_1.get())
+    	self.help_history_var_1.set(self.help_var.get())
+    	self.help_var.set(text)
+
 
     def add_inventory_item(self, item_name):
         next_available_inventory_slot = len(self.inventory_slots_in_use)
+
         if next_available_inventory_slot < len(self.inventory_slots):
             next_slot = self.inventory_slots[next_available_inventory_slot]
+            next_label_var = self.item_label_vars[next_available_inventory_slot]
 
             if item_name == "key":
                 next_slot.configure(image=self.key_image)
 
+            next_label_var.set(item_name.title())  # coloca a primeira letra em maiuscula
             self.inventory_slots_in_use.append(item_name)
+
+
+    def use_item(self, item_number):
+
+    	if item_number < len(self.inventory_slots_in_use):
+    		item_name = self.inventory_slots_in_use[item_number]
+
+    		if item_name:
+    			self.item_in_use = item_name
+
+    			for button in self.inventory_slots:
+    				button.configure(bg="black")
+
+    			self.inventory_slots[item_number].configure(bg="white")
+    			self.inventory_slots[item_number].configure(command=self.clear_used_item)
+
+
+    def clear_used_item(self):
+    	self.item_in_use = ""
+
+    	for index, button in enumerate[self.inventory_slots]:
+    		button.configure(bg="black")
+    		use_cmd = partial(self.use_item, item_number=index)  # 'partial', fixa o argumento item_number do método use_item
+    		button.configure(command=use_cmd)
+
+
+    def show_cannot_use_message(self):
+    	text_id = self.screen.create_text((250, 25), 
+    		                              text="Este item não pode ser usado aqui!",
+    		                              font=self.cannot_use_font,
+    		                              fill="white")
+    	self.after(2000, lambda: self.screen.delete(text_id))
+
+
 
     def play(self):
         if not self.game_screens:
@@ -131,12 +230,13 @@ class Game(tk.Tk):
             self.display_screen(0)
 
 
+
 if __name__ == "__main__":
     game = Game()
 
     scene1 = GameScreen(game, "assets/bedroom1.png", (378, 135, 427, 217), "key", "Você tem que sair, "
                                                                                 "mas a porta está fechada!\n Encontre a chave.")
-    scene2 = GameScreen(game, "assets/scene2.png", (117, 54, 329, 412), None, "Você tem a chave!")
+    scene2 = GameScreen(game, "assets/scene2.png", (117, 54, 329, 412), None, "Você tem a chave!", "chave")
     scene3 = GameScreen(game, "assets/scene3.png", (117, 54, 329, 412), None, "A porta está aberta.")
 
     all_screens = [scene1, scene2, scene3]
